4f655d61ab97a67aea5521b21e84e3784d61d822
Patrick Hieber <patrickhieber92@gmail.com>
2020-05-01 15:37:32 +0000

    (feat) Complete upload_docker.sh and rename image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=pckhib/udacity-devops-app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login
+docker tag udacity-devops-app $dockerpath
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
