e3d757c8931ad25248fdc9ca59460d4d902c90f5
Patrick Hieber <patrickhieber92@gmail.com>
2020-05-01 16:14:05 +0000

    (feat) Complete run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=pckhib/udacity-devops-app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run app --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward app 8000:80
